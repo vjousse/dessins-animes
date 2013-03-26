d87305a9381bb7fdb5be7d9d821644ed13178a2f
Vincent Jousse <vincent@jousse.org>
2013-03-26 14:49:09 +0100

    Add dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -2,10 +2,22 @@ import sbt._
 import Keys._
 import play.Project._
 
-object ApplicationBuild extends Build {
+trait Resolvers {
+  val codahale = "repo.codahale.com" at "http://repo.codahale.com/"
+  val typesafe = "typesafe.com" at "http://repo.typesafe.com/typesafe/releases/"
+}
+
+trait Dependencies {
+  val specs2 = "org.specs2" %% "specs2" % "1.12"
+  val postgres = "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+}
+
+object ApplicationBuild extends Build with Resolvers with Dependencies {
 
   val appName         = "dessins-animes"
   val appVersion      = "1.0-SNAPSHOT"
+
+
 
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -15,8 +27,26 @@ object ApplicationBuild extends Build {
   )
 
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
+  private val buildSettings = Project.defaultSettings ++ Seq(
+    organization := "com.dessins-animes",
+    version := appVersion,
+    resolvers := Seq(),
+    libraryDependencies := Seq(
+      jdbc,
+      anorm,
+      postgres),
+    libraryDependencies in test := Seq(specs2),
+    shellPrompt := {
+      (state: State) ⇒ "%s> ".format(Project.extract(state).currentProject.id)
+    },
+    scalacOptions := Seq("-deprecation", "-unchecked"),
+    testOptions in Test += Tests.Argument("junitxml", "console")
+  )
+  val da = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
   )
 
+  val cli = Project("cli", file("cli"), settings = buildSettings).settings(
+    libraryDependencies ++= Seq()
+  ) dependsOn (da)
 }
