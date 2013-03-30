db496789e874936e51d94ee522d8e23dea9f0420
Vincent Jousse <vincent@jousse.org>
2013-03-30 15:37:14 +0100

    Use slug in URL

--- app/controllers/Da.scala
+++ app/controllers/Da.scala
@@ -9,7 +9,7 @@ import da.apis.DaApi
 
 object Da extends Controller {
 
-  def show(id: Long) = Action {
+  def show(id: Long, dir: String) = Action {
     DB.withConnection { implicit conn =>
       DaApi.getDaById(id) match {
         case Some(da) => Ok(views.html.showDa(da))
